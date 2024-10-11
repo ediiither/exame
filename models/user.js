b71d7b3b2f4acb437d43494a88bb3d485269aa79
ediiither <edithfuentes511@gmail.com>
2024-10-11 10:03:10 -0400

    first commit

---
+++ models/user.js
@@ -0,0 +1,12 @@
+// User.js
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  age: { type: Number, optional: true },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
